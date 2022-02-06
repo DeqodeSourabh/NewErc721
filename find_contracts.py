40126dbc73302006041d0f6a43433b50bbf84c45
SourabhDeqode <sogupta@deqode.com>
2022-02-06 22:18:18 +0530

    changes in find_contracts code

--- find_contracts.py
+++ find_contracts.py
@@ -5,7 +5,6 @@ from web3.middleware import geth_poa_middleware
 import asyncio
 import ray
 import os
-from find_holders import C
 import pymongo
 from ray.util import inspect_serializability
 
@@ -13,13 +12,33 @@ infura_url= "wss://mainnet.infura.io/ws/v3/57d8e5ec16764a3e86ce18fc505e640e"
 web3 = Web3(Web3.WebsocketProvider(infura_url))
 web3.middleware_onion.inject(geth_poa_middleware, layer=0) 
 
+connection_url = 'mongodb+srv://sourabh:sourabh@cluster0.il3sa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+client = pymongo.MongoClient(connection_url)
+Database = client.get_database('myFirstDB')
+
+
+
+def mongoDb(address, fromBlock, toBlock):
+    connection_url = 'mongodb+srv://sourabh:sourabh@cluster0.il3sa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    client = pymongo.MongoClient(connection_url)
+    Database = client.get_database('myFirstDB')
+    fromToTable = Database.fromToTable
+    queryObject = {
+                'contractAddress': address,
+                'limitBlocks':{
+                'from':fromBlock,
+                'to': toBlock
+                }
+            }
+    print(address, fromBlock,toBlock)
+    fromToTable.insert_one(queryObject)
+
+
+
 @ray.remote
 def holdersContract(address,block):
     web3 = Web3(Web3.WebsocketProvider(infura_url))
     web3.middleware_onion.inject(geth_poa_middleware, layer=0) 
-
-    fromToTable = Database.fromToTable
-    
     abi = json.load(open('erc721.json','r'))
     contract = web3.eth.contract(address=address,abi=abi)
     latest = web3.eth.blockNumber
@@ -30,53 +49,26 @@ def holdersContract(address,block):
         while totalResult>=2000:
             fromBlock = initial
             toBlock = initial +2000
-            #holdersEvent.remote(fromBlock,toBlock,contract)
-
-# --------------------------- yaha se samajh --------------------------------------------
-    
-            if fromToTable.find_one({'contractAddress': address}) == None:
-                fromToTable.insert_one(queryObject)
-                queryObject = {
-                    'contractAddress': address,
-                    'limitBlocks':{
-                    'from':fromBlock,
-                    'to': toBlock
-                    }
-                }
-            else:
-                queryObject.limitBlocks={
-                    'from': fromBlock,
-                    'to': toBlock
-                }
-            fromToTable.insert_one(queryObject)
-
+            #holdersEvent.remote(fromBlock,toBlock,contract
+            mongoDb(address, fromBlock, toBlock)
             totalResult = totalResult -2000
             initial = toBlock
         if totalResult != 0:
             fromBlock = initial
             toBlock = initial + totalResult
             #holdersEvent.remote(fromBlock,toBlock,contract)
-            print("2")
+            mongoDb(address, fromBlock, toBlock)
     else:
-        queryObject = {
-                    'contractAddress': address,
-                    'limitBlocks':{
-                    'from':firstBlock,
-                    'to': latest
-                    }
-                }
-        fromToTable.insert_one(queryObject)
-        print(firstBlock,latest)
+        mongoDb(address, firstBlock, latest)
         #c.holdersEvent.remote(firstBlock,latest,contract)
-    #------------------------------------------------------------------------
+    return 0
 
 inspect_serializability(holdersContract, name="contract")   
-connection_url = 'mongodb+srv://sourabh:sourabh@cluster0.il3sa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-client = pymongo.MongoClient(connection_url)
-Database = client.get_database('myFirstDB')
-if __name__=="__main__":
-    ray.init()
 
+if __name__== "__main__":
+    ray.init()
+    futures=[]
+    Database = client.get_database('myFirstDB')
     SampleTable = Database.SampleTable
     query = SampleTable.find()
     output={}
@@ -86,4 +78,5 @@ if __name__=="__main__":
         details.append(x)
     for l in details:
         block = web3.eth.getTransactionReceipt(l['tx_hash']).blockNumber
-        holdersContract.remote(l['contractAddress'], block)
+        futures.append(holdersContract.remote(l['contractAddress'], block))
+    ray.get(futures)
